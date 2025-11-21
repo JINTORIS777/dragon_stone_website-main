f961c3224e1f994bc34b9e77a914eb5dbdbdef99
Karabo Masike <kmasike2001@gmail.com>
2025-11-21 14:22:44 +0200

    Delete dragon_stone_website_main directory

--- dragon_stone_website_main/cancel.php
+++
@@ -1,4 +0,0 @@
-<!doctype html><html><head><meta charset="utf-8"><title>Payment Canceled</title></head><body>
-<h2>Payment Canceled</h2>
-<p>Your payment was canceled. <a href="checkout.php">Return to checkout</a></p>
-</body></html>
--- dragon_stone_website_main/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__.'/server/auth.php';
-session_start();
-session_unset(); session_destroy();
-header('Location: index.php'); exit;
-?>
